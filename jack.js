975ead864d0dd85f10709e1a3dae4940c2c9cf2a
Jack Curtin <jack23curtin@gmail.com>
2018-03-06 18:24:25 -0500

    Update jack.js

--- jack.js
+++ jack.js
@@ -1,7 +1,9 @@
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
-  } else {
-    $('nav').removeClass('shrink');
-  }
-});
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
