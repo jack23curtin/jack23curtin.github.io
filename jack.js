4d968c2b079504bfedf3e4ab344bc70a82dcd857
Jack Curtin <jack23curtin@gmail.com>
2018-03-06 18:42:41 -0500

    Update jack.js

--- jack.js
+++ jack.js
@@ -1,7 +1,1 @@
-$(document).on("scroll",function(){
-    if($(document).scrollTop()>100){
-        $("header").removeClass("large").addClass("small");
-    } else{
-        $("header").removeClass("small").addClass("large");
-    }
-});
+
