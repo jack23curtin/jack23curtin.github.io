c7aae3443f59e671b011f82262c152a772265561
Jack Curtin <jack23curtin@gmail.com>
2018-06-13 11:54:26 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,4 +3,26 @@ $id = filter_input(INPUT_POST,'id');
 $title = filter_input(INPUT_POST,'title');
 $author = filter_input(INPUT_POST,'author');
 $date = filter_input(INPUT_POST,'date');
+     
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "testFile_db";
+
+//Connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+     
+if (mysqli_connect_error()){
+     die('Connect Error ('.mysqli_connect_error().')'.mysqli_connect_error());
+     }
+else{
+     $sql = "INSERT INTO documentinfo (id, title, author, date) values ('$id','$title','$author','$date')";
+     if ($conn->query($sql)){
+        echo "New record is inserted sucessfully";
+      }
+      else{
+        echo "Error: " . $sql. "<br>". $conn->error;
+      }
+      $conn->close();
+     }
 ?>
